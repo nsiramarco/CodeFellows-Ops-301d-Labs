cc5c0c2de76e6e0d94bac8b1bd0ed411acdcf49e
github_username <you@example.com>
2023-06-01 08:47:58 -0400

    pending

---
+++ Ch02.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       NATASHA SIRAMARCO
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+
+# End
