ec0ddc3dee8db762decbe07915ec468e5175f1e1
github_username <you@example.com>
2023-06-01 08:55:49 -0400

    pending 1

--- Ch02.sh
+++ Ch02.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Script Name:                  Script Name
+# Script Name:                  Ops Challenge: Append; Date and Time
 # Author:                       NATASHA SIRAMARCO
-# Date of latest revision:      00/00/0000
-# Purpose:                      Purpose
+# Date of latest revision:      06/01/2023
+# Purpose:                      Manipulate a variable in bash to apply today’s date to a log file. 
+#                               Resource: [Shell style guide](https://google.github.io/styleguide/shellguide.html)
 
 # Declaration of variables
 
