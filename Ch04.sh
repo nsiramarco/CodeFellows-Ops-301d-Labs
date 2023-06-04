176ab7041d159bd553d6346ea2537378b586de03
github_username <you@example.com>
2023-06-04 19:02:11 -0400

    Challenge 04

--- Ch04.sh
+++ Ch04.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Script Name:                  Script Name
+# Script Name:                  Create Conditionals in Menu Systems
 # Author:                       NATASHA SIRAMARCO
-# Date of latest revision:      00/00/0000
-# Purpose:                      Purpose
+# Date of latest revision:      06/03/2023
+# Purpose:                      Create a bash script that launches a menu system with the following options:
+#                               Hello world (prints “Hello world!” to the screen)
+#                               Ping self (pings this computer’s loopback address)
+#                               IP info (print the network adapter information for this computer)
+#                               Exit
+#                               Next, the user input should be requested.
+#                               The program should next use a conditional statement to evaluate the user’s input, then act according to what the user selected.
+#                               Use a loop to bring up the menu again after the request has been executed.
 
 # Declaration of variables
 
@@ -11,7 +18,36 @@
 
 
 # Main
+while true; do
+    echo "Select a choice:"
+    echo "1 for Hello World!"
+    echo "2 for self ping"
+    echo "3 for IP Information"
+    echo "4 to Exit"
+    read -p "Please enter your selection: " choice
 
+    case $choice in
+        1)
+            echo "Hello World!!!"
+            ;;
+        2)
+            ping -c 2 127.0.0.1
+            ;;
+        3)
+            ip addr show
+            ;;
+        4)
+            echo "Now Exiting..."
+            echo "Complete"
+            exit 0
+            ;;
+        *)
+            echo "This is not a selection. Please try again..."
+            ;;
+    esac
+
+    echo
+done
 
 
 # End
