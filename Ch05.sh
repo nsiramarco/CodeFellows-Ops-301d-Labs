8a7a994bb747991d0421554b43cfaf35f1f2ac58
github_username <you@example.com>
2023-06-06 15:48:15 -0400

    Challenge 05

--- Ch05.sh
+++ Ch05.sh
@@ -22,25 +22,30 @@
 # Main
 # Print to the screen the file size
 echo "File Size and Name:"
-du -h /var/log/syslog /var/log/wtmp
+stat -c '%s' /var/log/syslog /var/log/wtmp
+
 echo 
 # Compress Files into Backup Directory and TimeStamp
 echo 
 zip -r Backup-Directory$(date +"%Y%m%d%H%M%S").zip /var/log/syslog /var/log/wtmp
 echo "Backup has been Created..."
 echo 
+
 # Print compressed file sizes
 echo "Compressed File Size:"
 gzip -l Backup-Directory$(date +"%Y%m%d%H%M%S").zip
 echo
 
-# Clear Content
-> sudo /var/log/syslog /var/log/wtmp
+# Clear File Content
+sudo truncate -s 0 /var/log/syslog /var/log/wtmp
 
 # Compare sizes
 echo "Comparison:"
 du -h /var/log/syslog /var/log/wtmp
 du -h Backup-Directory$(date +"%Y%m%d%H%M%S").zip
 echo 
+
+
+
 
 # End
