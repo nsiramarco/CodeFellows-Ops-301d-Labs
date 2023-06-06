087747708f7aaf13ced5b8d6b0c04ce277e487c0
github_username <you@example.com>
2023-06-06 10:02:11 -0400

    partial

--- Ch05.sh
+++ Ch05.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# Script Name:                  Script Name
+# Script Name:                  Clearing Logs
 # Author:                       NATASHA SIRAMARCO
-# Date of latest revision:      00/00/0000
-# Purpose:                      Purpose
+# Date of latest revision:      6/05/2023
+# Purpose:                      Write a bash script that performs the following tasks:
+#                               Print to the screen the file size of the log files before compression
+#                               Compress the contents of the log files listed below to a backup directory
+#                               /var/log/syslog
+#                               /var/log/wtmp
+#                               The file name should contain a time stamp with the following format -YYYYMMDDHHMMSS
+#                               Example: /var/log/backups/syslog-20220928081457.zip
+#                               Clear the contents of the log file
+#                               Print to screen the file size of the compressed file
+#                               Compare the size of the compressed files to the size of the original log files
 
 # Declaration of variables
 
@@ -11,7 +20,22 @@
 
 
 # Main
-
+# Print to the screen the file size
+echo "File Size and Name:"
+du -h /var/log/syslog /var/log/wtmp
+echo 
+# Compress Files into Backup Directory and TimeStamp
+echo 
+zip -r Backup-Directory$(date +"%Y%m%d%H%M%S").zip /var/log/syslog /var/log/wtmp
+echo "Backup has been Created..."
+# Print compressed file sizes
+echo "Compressed File Size:"
+gzip -l Backup-Directory$(date +"%Y%m%d%H%M%S").zip
+echo
+# Compare sizes
+echo "Comparison:"
+du -c /var/log/syslog /var/log/wtmp | awk '{print $1}' | bc -l
+du -c Backup-Directory$(date +"%Y%m%d%H%M%S").zip | awk '{print $1}' | bc -l
 
 
 # End
