9b7c2fd4b607230306577da9620661dbcedd914a
github_username <you@example.com>
2023-06-08 10:17:15 -0400

    Python Template

--- PythonTemplate.py
+++ PythonTemplate.py
@@ -10,18 +10,6 @@ import os
 
 # Declaration of functions
 
-### Declare a function here
-
-for (root, dirs, files) in os.walk("testdir"):
-    ### Add a print command here to print ==root==
-    print(root)
-    ### Add a print command here to print ==dirs==
-    print(dirs)
-    ### Add a print command here to print ==files==
-    print(files)
-
 # Main
-
-### Pass the variable into the function here
 
 # End
