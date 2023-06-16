6593512205dbe8b3704eac9b5bec3455d93e6669
github_username <you@example.com>
2023-06-16 10:43:41 -0400

    pending

---
+++ Ch13.ps1
@@ -0,0 +1,9 @@
+# Script Name:                  Script Name
+# Author:                       NATASHA SIRAMARCO
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# *****Key Note: this script is to be performed on the Powershell ISE*****
+# *(text) = powershell output
+New-ADUser
+*Name: 
