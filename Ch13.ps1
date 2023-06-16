564296ee6623654dceb06d3be5fda90dfc2ac53a
github_username <you@example.com>
2023-06-16 11:39:31 -0400

    Challenge 13

--- Ch13.ps1
+++ Ch13.ps1
@@ -1,9 +1,12 @@
-# Script Name:                  Script Name
+# Script Name:                  Powershell AD Automation
 # Author:                       NATASHA SIRAMARCO
-# Date of latest revision:      00/00/0000
-# Purpose:                      Purpose
+# Date of latest revision:      06/16/2023
+# Purpose:                      In your Windows Server, access Powershell ISE.               
+#                               Write a Powershell script that adds the below person to AD.
+#                               Franz Ferdinand is the TPS Reporting Lead at GlobeX USA in Springfield, OR office. 
+#                               Franz is part of the TPS Department. Franz’s email is ferdi@GlobeXpower.com.
 
 # *****Key Note: this script is to be performed on the Powershell ISE*****
-# *(text) = powershell output
-New-ADUser
-*Name: 
+
+New-ADUser -Name "Franz Ferdinand" -City Springfield -Company "GlobeX USA" -Department TPS -DisplayName "Franz Ferdinand" - EmailAddress ferdi@GlobeXpower.com -Office Springfield -State OR -Title "TPS Reporting Lead"
+
